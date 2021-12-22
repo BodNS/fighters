3c759b039e201d30fbef16b7277b585a54b4ff4e
BodNS <bondarenokn@gmail.com>
2021-12-22 17:44:01 +0200

    211222

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -4,14 +4,32 @@ export function createFighterPreview(fighter, position) {
   const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
   const fighterElement = createElement({
     tagName: 'div',
-    className: `fighter-preview___root ${positionClassName}`,
+    className: `fighter-preview___root ${positionClassName}`
   });
 
   // todo: show fighter info (image, name, health, etc.)
-  
-
+  const fighterInfo = [
+    createFighterText(fighter.name),
+    createFighterText("Health: "+ fighter.health),
+    createFighterText("Attack: "+ fighter.attack),
+    createFighterText("Defense: "+ fighter.defense)
+  ]
+  const fighterImage = createFighterImage(fighter);
+    
+  fighterElement.append(fighterImage, ...fighterInfo);
   return fighterElement;
 }
+
+export function createFighterText(txt) {
+  const txtElement = createElement({
+    tagName: 'p',
+    className: 'fighter-prewiew__txt',
+    
+  })
+  txtElement.textContent = txt;
+  return txtElement;
+}
+
 
 export function createFighterImage(fighter) {
   const { source, name } = fighter;
